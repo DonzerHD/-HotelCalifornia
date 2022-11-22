6529f6939d60d1866f0812125c2348ed49dee9b6
DonzerHD <thomas.lemay59@outlook.fr>
2022-11-22 16:39:16 +0100

    hotel

--- HotelCalifornia/src/hotel/Chambre.java
+++ HotelCalifornia/src/hotel/Chambre.java
@@ -1,5 +1,46 @@
 package hotel;
 
 public class Chambre {
+    // tableaux prix 
+	
+	private int[] prix1 = {100, 120, 130, 150};
+	private int[] prix2 = {130, 160, 170, 190};
+	private int[] prix3 = {170, 200, 210, 230, 270 , 350};
+	private int[] prix4 = {200, 230, 240, 260, 300 , 400};
+	
+    public réservationChambre{
+    	
+    }
+	
+
+	
+	
+	
+	public int[] getPrix1() {
+		return prix1;
+	}
+	public void setPrix1(int[] prix1) {
+		this.prix1 = prix1;
+	}
+	public int[] getPrix2() {
+		return prix2;
+	}
+	public void setPrix2(int[] prix2) {
+		this.prix2 = prix2;
+	}
+	public int[] getPrix3() {
+		return prix3;
+	}
+	public void setPrix3(int[] prix3) {
+		this.prix3 = prix3;
+	}
+	public int[] getPrix4() {
+		return prix4;
+	}
+	public void setPrix4(int[] prix4) {
+		this.prix4 = prix4;
+	}
+	
     
+	
 }
--- HotelCalifornia/src/hotel/Main.java
+++ HotelCalifornia/src/hotel/Main.java
@@ -2,6 +2,8 @@ package hotel;
 
 public class Main {
       public static void main(String[] args) {
-		
+    	  
+		   System.out.println("Bienvenue à Hotel-Carlifornia");
+		   System.out.println(Donn);
 	}
 }
